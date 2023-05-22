177857a974e2bbeb4180a7e59161d17cd41d92db
a-andrievskikh <sn0abg0@gmail.com>
2023-05-23 00:45:17 +0500

    feat: import TodoList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import './App.css'
+import { TodoList } from './TodoList'
 
-export function App() {
+export const App = () => {
+
+  const truck = `What to learn 1`
+  const truck2 = `What to learn 2`
+
+  const tasks1 = [
+    {id: 1, title: 'HTML&CSS', isDone: true},
+    {id: 2, title: 'JS', isDone: true},
+    {id: 3, title: 'ReactJS', isDone: false},
+  ]
+  const tasks2 = [
+    {id: 1, title: 'Hello world', isDone: true},
+    {id: 2, title: 'I am Happy', isDone: false},
+    {id: 3, title: 'Yo', isDone: false},
+  ]
+
   return (
     <div className="App">
-      <div>
-        <h3>What to learn</h3>
-        <div>
-          <input />
-          <button>+</button>
-        </div>
-        <ul>
-          <li><input type="checkbox" checked={true} /> <span>HTML&CSS</span></li>
-          <li><input type="checkbox" checked={true} /> <span>JS</span></li>
-          <li><input type="checkbox" checked={false} /> <span>React</span></li>
-        </ul>
-        <div>
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
-        </div>
-      </div>
+      <TodoList truck={truck} truck2={10200} tasks={tasks1} />
+      <TodoList truck={truck2} truck3={true} tasks={tasks2} />
     </div>
   )
 }
